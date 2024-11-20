adca5acec604993af1d2b129d4eb3c4c9705b385
Venkata Pavan <venkata.p@trundl.com>
2024-11-20 17:13:42 +0530

    resolved the api error

--- src/GoogleMapsLoader.jsx
+++ src/GoogleMapsLoader.jsx
@@ -2,16 +2,9 @@ import React from 'react';
 import { LoadScript } from '@react-google-maps/api';
 
 const GoogleMapsLoader = ({ children }) => {
-  const googleMapsApiKey = process.env.GOOGLE_API_KEY; // Correctly prefixed environment variable
-
-  if (!googleMapsApiKey) {
-    console.error('Google Maps API Key is missing');
-    return null;
-  }
-
   return (
     <LoadScript
-      googleMapsApiKey={googleMapsApiKey}
+      googleMapsApiKey="AIzaSyCKvGu08e8T26TiPih8JH-lyGUJcuKgILY"
       libraries={['places']} // Load the places library for Autocomplete functionality
     >
       {children}
